9c48470141f6ea346d7d6e3e6e3539673aa67b9c
innerviewer <innerviewerr@gmail.com>
2024-04-01 20:47:18 +0200

    (CI) Fixed syntax in the script.

--- CI/BackupCI.py
+++ CI/BackupCI.py
@@ -1,7 +1,8 @@
-print("Backup.py script is running...")
-
 import os
-import re
+import subprocess
+import sys
+
+print("Backup.py script is running...")
 
 try:
     from github import Github
@@ -18,28 +19,27 @@ try:
     print("Trying to login to GitHub.")
     auth = Auth.Token(token)
     backup_g = Github(auth=auth)
-	backup_org = backup_g.get_user('SpaRcle-Studio-Backup')
-	backup_repos = backup_org.get_repos()
+    backup_org = backup_g.get_user('SpaRcle-Studio-Backup')
+    backup_repos = backup_org.get_repos()
 except Exception as e:
     print(f"GitHub login exception occurred: {str(e)}")
     exit(0)
-	
-g = Github() 
-organization = g.get_user('Sparcle-Studio')
+
+g = Github()
+organization = g.get_user('SpaRcle-Studio')
 repos = organization.get_repos()
 
 backup_repos_names = []
 for backup_repo in backup_repos:
-	backup_repos_names.append(backup_repo.full_name)
+    backup_repos_names.append(backup_repo.full_name)
 
-for repo in repos: 
-	if repo.full_name in backup_repos_names:
-		continue
-	
-	fork = backup_org.create_fork(repo, default_branch_only=False)
-	
-#backup_repos = backup_org.get_repos()
+for repo in repos:
+    if repo.full_name in backup_repos_names:
+        continue
 
-#for backup_repo in backup_repos: 
-#	if backup_repo.fork:
-		
+    fork = backup_org.create_fork(repo, default_branch_only=False)
+
+# backup_repos = backup_org.get_repos()
+
+# for backup_repo in backup_repos:
+#   if backup_repo.fork:
